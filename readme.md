52b155b183fad5639b6189c32b64aafcf42c7a41
jared <jtruscott78@gmail.com>
2019-02-14 15:47:10 -0500

    landing page complete

--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 # portfolio
 
-wire frame(ish)
+currently live at
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/3c09052f-757c-460a-bc70-6eb6ef0e6132/deploy-status)](https://app.netlify.com/sites/jaredmakes/deploys)
+
+eventually will replace
+
+https://jaredmakes.website ❤️
--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,1 +1,9 @@
-console.log('Working!');
+window.addEventListener('touchstart', () => {
+  const landingPageLink = document.querySelectorAll('.landing-page__link');
+  landingPageLink.forEach(e => (e.style.filter = 'blur(0)'));
+});
+
+// window.addEventListener('touchend', () => {
+//   const landingPageLink = document.querySelectorAll('.landing-page__link');
+//   landingPageLink.forEach(e => (e.style.filter = 'blur(1px)'));
+// });
