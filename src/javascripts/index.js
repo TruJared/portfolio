6382b2c27513615ec22d0a87dba5b6244c07d2d2
Jared <jtruscott78@gmail.com>
2019-02-14 23:19:30 -0500

    basic nav complete

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,9 +1,1 @@
-window.addEventListener('touchstart', () => {
-  const landingPageLink = document.querySelectorAll('.landing-page__link');
-  landingPageLink.forEach(e => (e.style.filter = 'blur(0)'));
-});
-
-// window.addEventListener('touchend', () => {
-//   const landingPageLink = document.querySelectorAll('.landing-page__link');
-//   landingPageLink.forEach(e => (e.style.filter = 'blur(1px)'));
-// });
+console.log('working');
